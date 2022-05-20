b52737c5c1d2b3858aac7a8807dc1e9ece893dbc
Ksenia KOZIAIKINA <100062948+XeniaCat@users.noreply.github.com>
2022-05-20 21:46:37 +0300

    Update my.js

--- my.js
+++ my.js
@@ -4,6 +4,17 @@ let inputPhone = document.getElementById('phone');
 let div = document.getElementById('li');
 let ul = document.createElement('ul');
 div.appendChild(ul);
+
+if (localStorage.length)  {
+    for (let i = 0; i < localStorage.length; i++ ) {
+    const li = document.createElement('li');
+    ul.appendChild(li);
+    let m = {}
+     m = JSON.parse(localStorage.getItem(`user${i}`));
+    li.innerHTML = `${m.name} - ${m.phone}` ;
+    li.id = `user${i}` 
+     };
+};
 // Event click on button
 button.addEventListener('click', function (event) {
     event.preventDefault();
@@ -17,24 +28,23 @@ button.addEventListener('click', function (event) {
         localStorage.setItem(`user${localStorage.length}`, JSON.stringify(user));
         inputName.value = '';
         inputPhone.value = '';
-        let m = null
-        for (let i = 0; i < localStorage.length; i++) {
-            const li = document.createElement('li');
-            ul.appendChild(li);
-            m =localStorage.getItem(`user${i}`);
-            li.innerHTML = m;
-          }
+        const li = document.createElement("li");
+        ul.appendChild(li);
+        li.id = `user${localStorage.length - 1}` ;
+        li.innerHTML = `${user.name} - ${user.phone}` ;
     } else {
         alert('Fill the form');
     }
 });
 document.querySelector('.delete-all').onclick = myClick;
 function myClick() {
-    localStorage.clear()
-    ul.innerHTML = ''
+    localStorage.clear() ;
+    ul.innerHTML = '' ;
 };
-li.onclick = click
-function click () {
-    li.innerHTML = ''
-    localStorage.removeItem
-}
+li.onclick = click ;
+function click (event) {
+console.log(event.target)
+let del = document.getElementById(event.target.id)
+del.remove()
+localStorage.removeItem(event.target.id)
+};
